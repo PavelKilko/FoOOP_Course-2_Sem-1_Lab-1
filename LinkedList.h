6f7c32e462aab954518883e54691508a782c00d9
PavelKilko <pavel.kilko@gmail.com>
2021-10-05 21:53:02 +0300

    Declared ArrayList class

--- LinkedList.h
+++ LinkedList.h
@@ -5,4 +5,14 @@
 #ifndef LAB1_LINKEDLIST_H
 #define LAB1_LINKEDLIST_H
 
+template <class T>
+class ArrayList
+{
+private:
+    int length;
+    int frontPos;
+    int backPos;
+    T array[100000];
+};
+
 #endif //LAB1_LINKEDLIST_H
