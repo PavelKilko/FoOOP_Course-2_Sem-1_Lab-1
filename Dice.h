e2c9f162a7a7fc586738daf0cd55fd681d045074
PavelKilko <pavel.kilko@gmail.com>
2021-10-06 00:49:33 +0300

    Declared Dice class

--- Dice.h
+++ Dice.h
@@ -5,4 +5,35 @@
 #ifndef LAB1_DICE_H
 #define LAB1_DICE_H
 
+#include "List.h"
+
+class Dice
+{
+private:
+    int countOfSides;
+    double fullProbability;
+    class Side
+    {
+    public:
+        int number;
+        double dropProbability;
+
+        Side()
+        {
+            number = 1;
+            dropProbability = 0;
+        }
+
+        Side(int n, double dP)
+        {
+            if (n < 0) n = 1;
+            if (dP > 1) dP = 1;
+            if (dP < 0) dP = 0;
+            number = n;
+            dropProbability = dP;
+        }
+    };
+    LinkedList<Side> sides;
+};
+
 #endif //LAB1_DICE_H
